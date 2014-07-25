43c64629e1cfb8848de7b7a4009370d01c3ef313
Andrew Roth <aroth2@brocku.ca>
2014-07-25 18:27:07 -0400

    Trimming the fat
    hacking and slashing

--- jit.artoolkit.c
+++ jit.artoolkit.c
@@ -6,50 +6,24 @@
 #include "jit.common.h"
 #include "ext_systhread.h"
 
-typedef struct _jit_artoolkit_vecdata_char
-{
-	long				min[JIT_MATRIX_MAX_PLANECOUNT];
-	long 				mean[JIT_MATRIX_MAX_PLANECOUNT];
-	long 				max[JIT_MATRIX_MAX_PLANECOUNT];
-} t_jit_artoolkit_vecdata_char;
-
-typedef struct _jit_artoolkit_vecdata_long
-{
-	long				min[JIT_MATRIX_MAX_PLANECOUNT];
-	double 				mean[JIT_MATRIX_MAX_PLANECOUNT];
-	long				max[JIT_MATRIX_MAX_PLANECOUNT];
-} t_jit_artoolkit_vecdata_long;
-
-typedef struct _jit_artoolkit_vecdata_float32
-{
-	float				min[JIT_MATRIX_MAX_PLANECOUNT];
-	float 				mean[JIT_MATRIX_MAX_PLANECOUNT];
-	float				max[JIT_MATRIX_MAX_PLANECOUNT];
-} t_jit_artoolkit_vecdata_float32;
-
-typedef struct _jit_artoolkit_vecdata_float64
-{
-	double				min[JIT_MATRIX_MAX_PLANECOUNT];
-	double 				mean[JIT_MATRIX_MAX_PLANECOUNT];
-	double				max[JIT_MATRIX_MAX_PLANECOUNT];
-} t_jit_artoolkit_vecdata_float64;
-
-typedef struct _jit_artoolkit_vecdata
-{
-	t_jit_artoolkit_vecdata_char 		v_char;
-	t_jit_artoolkit_vecdata_long 		v_long;
-	t_jit_artoolkit_vecdata_float32 	v_float32;
-	t_jit_artoolkit_vecdata_float64 	v_float64;
-} t_jit_artoolkit_vecdata;
+#include <stdio.h>
+#include <stdlib.h>					// malloc(), free()
+#ifdef __APPLE__
+#  include <GLUT/glut.h>
+#else
+#  include <GL/glut.h>
+#endif
+#include "AR/config.h"
+#include "AR/video.h"
+#include "AR/param.h"			// arParamDisp()
+#include "AR/ar.h"
+#include "AR/gsub_lite.h"
 
 typedef struct _jit_artoolkit 
 {
 	t_object	ob;
 	long		planecount;
-	t_atom		min[JIT_MATRIX_MAX_PLANECOUNT];
-	t_atom		mean[JIT_MATRIX_MAX_PLANECOUNT];
-	t_atom		max[JIT_MATRIX_MAX_PLANECOUNT];
-	t_jit_artoolkit_vecdata vd;
+	//t_jit_artoolkit_vecdata vd;
 	t_systhread_mutex mutex;
 } t_jit_artoolkit;
 
@@ -60,15 +34,8 @@ void *_jit_artoolkit_class;
 
 t_jit_artoolkit *jit_artoolkit_new(void);
 void jit_artoolkit_free(t_jit_artoolkit *x);
-void jit_artoolkit_precalc(t_jit_artoolkit_vecdata *vecdata, t_jit_matrix_info *in1_minfo, char *bip1); 
-void jit_artoolkit_postcalc(t_jit_artoolkit *x, t_jit_artoolkit_vecdata *vecdata, t_jit_matrix_info *in1_minfo); 
-void jit_artoolkit_mean(t_jit_artoolkit_vecdata *vecdata, t_jit_matrix_info *in1_minfo); 
 void jit_artoolkit_calculate_ndim(t_jit_artoolkit *x, long dimcount, long *dim, long planecount, 
 	t_jit_matrix_info *in1_minfo, char *bip1);
-void jit_artoolkit_vector_char(long n, t_jit_op_info *in1, long *min, long *mean, long *max); 
-void jit_artoolkit_vector_long(long n, t_jit_op_info *in1, long *min, double *mean, long *max);
-void jit_artoolkit_vector_float32(long n, t_jit_op_info *in1, float *min, float *mean, float *max);
-void jit_artoolkit_vector_float64(long n, t_jit_op_info *in1, double *min, double *mean, double *max);;
 
 t_jit_err jit_artoolkit_init(void) 
 {
@@ -88,25 +55,7 @@ t_jit_err jit_artoolkit_init(void)
 
 	CLASS_STICKY_CATEGORY(_jit_artoolkit_class,0,"Value");
 
-	attr = jit_object_new(_jit_sym_jit_attr_offset_array,"min",_jit_sym_atom,JIT_MATRIX_MAX_PLANECOUNT,attrflags,
-		(method)0L,(method)0L,calcoffset(t_jit_artoolkit,planecount),calcoffset(t_jit_artoolkit,min));
-	jit_class_addattr(_jit_artoolkit_class,attr);
-	CLASS_ATTR_BASIC(_jit_artoolkit_class,"min",0);
-	CLASS_ATTR_LABEL(_jit_artoolkit_class,"min",0,"Minimum");
-
-	attr = jit_object_new(_jit_sym_jit_attr_offset_array,"mean",_jit_sym_atom,JIT_MATRIX_MAX_PLANECOUNT,attrflags,
-		(method)0L,(method)0L,calcoffset(t_jit_artoolkit,planecount),calcoffset(t_jit_artoolkit,mean));
-	jit_class_addattr(_jit_artoolkit_class,attr);
-	CLASS_ATTR_BASIC(_jit_artoolkit_class,"mean",0);
-	CLASS_ATTR_LABEL(_jit_artoolkit_class,"mean",0,"Mean");
-
-	attr = jit_object_new(_jit_sym_jit_attr_offset_array,"max",_jit_sym_atom,JIT_MATRIX_MAX_PLANECOUNT,attrflags,
-		(method)0L,(method)0L,calcoffset(t_jit_artoolkit,planecount),calcoffset(t_jit_artoolkit,max));
-	jit_class_addattr(_jit_artoolkit_class,attr);
-	CLASS_ATTR_BASIC(_jit_artoolkit_class,"max",0);
-	CLASS_ATTR_LABEL(_jit_artoolkit_class,"max",0,"Maximum");
-	
-	CLASS_STICKY_CATEGORY_CLEAR(_jit_artoolkit_class);
+    CLASS_STICKY_CATEGORY_CLEAR(_jit_artoolkit_class);
 	
 	jit_class_register(_jit_artoolkit_class);
 
@@ -140,12 +89,12 @@ t_jit_err jit_artoolkit_matrix_calc(t_jit_artoolkit *x, void *inputs, void *outp
 		}		
 		
 		//calculate
-		jit_artoolkit_precalc(&x->vd, &in_minfo, in_bp);
+		//jit_artoolkit_precalc(&x->vd, &in_minfo, in_bp);
 		jit_parallel_ndim_simplecalc1((method)jit_artoolkit_calculate_ndim, 
 			x, dimcount, dim, in_minfo.planecount, &in_minfo, in_bp,
 			0 /* flags1 */);
-		jit_artoolkit_mean(&x->vd, &in_minfo);
-		jit_artoolkit_postcalc(x, &x->vd, &in_minfo);
+		//jit_artoolkit_mean(&x->vd, &in_minfo);
+		//jit_artoolkit_postcalc(x, &x->vd, &in_minfo);
 		
 	} else {
 		return JIT_ERR_INVALID_PTR;
@@ -156,271 +105,12 @@ out:
 	return err;
 }
 
-void jit_artoolkit_precalc(t_jit_artoolkit_vecdata *vecdata, t_jit_matrix_info *in1_minfo, char *bip1) 
-{
-	long i;
-	
-	if (in1_minfo->type==_jit_sym_char) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_char.min[i]  = ((uchar *)bip1)[i];
-			vecdata->v_char.max[i]  = ((uchar *)bip1)[i];
-			vecdata->v_char.mean[i] = 0;
-		}
-	} else if (in1_minfo->type==_jit_sym_long) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_long.min[i]  = ((t_int32 *)bip1)[i];
-			vecdata->v_long.max[i]  = ((t_int32 *)bip1)[i];
-			vecdata->v_long.mean[i] = 0;
-		}
-	} else if (in1_minfo->type==_jit_sym_float32) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_float32.min[i]  = ((float *)bip1)[i];
-			vecdata->v_float32.max[i]  = ((float *)bip1)[i];
-			vecdata->v_float32.mean[i] = 0;
-		}
-	} else if (in1_minfo->type==_jit_sym_float64) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_float64.min[i]  = ((double *)bip1)[i];
-			vecdata->v_float64.max[i]  = ((double *)bip1)[i];
-			vecdata->v_float64.mean[i] = 0;
-		}
-	} 
-}
-
-void jit_artoolkit_postcalc(t_jit_artoolkit *x, t_jit_artoolkit_vecdata *vecdata, t_jit_matrix_info *in1_minfo) 
-{
-	long i;
-	
-	x->planecount = in1_minfo->planecount;
-
-	if (in1_minfo->type==_jit_sym_char) { 
-		for (i=0;i<x->planecount;i++) {
-			jit_atom_setlong(&(x->min[i]),vecdata->v_char.min[i]);
-			jit_atom_setfloat(&(x->mean[i]),vecdata->v_long.mean[i]); //(hack) use v_long's float mean
-			jit_atom_setlong(&(x->max[i]),vecdata->v_char.max[i]);
-		}
-	} else if (in1_minfo->type==_jit_sym_long) { 
-		for (i=0;i<x->planecount;i++) {
-			jit_atom_setlong(&(x->min[i]),vecdata->v_long.min[i]);
-			jit_atom_setfloat(&(x->mean[i]),vecdata->v_long.mean[i]);
-			jit_atom_setlong(&(x->max[i]),vecdata->v_long.max[i]);
-		}
-	} else if (in1_minfo->type==_jit_sym_float32) { 
-		for (i=0;i<x->planecount;i++) {
-			jit_atom_setfloat(&(x->min[i]),vecdata->v_float32.min[i]);
-			jit_atom_setfloat(&(x->mean[i]),vecdata->v_float32.mean[i]);
-			jit_atom_setfloat(&(x->max[i]),vecdata->v_float32.max[i]);
-		}
-	} else if (in1_minfo->type==_jit_sym_float64) { 
-		for (i=0;i<x->planecount;i++) {
-			jit_atom_setfloat(&(x->min[i]),vecdata->v_float64.min[i]);
-			jit_atom_setfloat(&(x->mean[i]),vecdata->v_float64.mean[i]);
-			jit_atom_setfloat(&(x->max[i]),vecdata->v_float64.max[i]);
-		}
-	}
-}
-
-void jit_artoolkit_mean(t_jit_artoolkit_vecdata *vecdata, t_jit_matrix_info *in1_minfo) 
-{
-	long count=1;
-	long i;
-	
-	for (i=0;i<in1_minfo->dimcount;i++) {
-		count *= in1_minfo->dim[i];
-	}
-			
-	if (in1_minfo->type==_jit_sym_char) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_long.mean[i] = ((double)(vecdata->v_char.mean[i]))/(double)count; //(hack) use v_long's float mean
-		}
-	} else if (in1_minfo->type==_jit_sym_long) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_long.mean[i] = ((double)(vecdata->v_long.mean[i]))/(double)count;
-		}
-	} else if (in1_minfo->type==_jit_sym_float32) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_float32.mean[i] = ((double)(vecdata->v_float32.mean[i]))/(double)count;
-		}
-	} else if (in1_minfo->type==_jit_sym_float64) {
-		for (i=0;i<in1_minfo->planecount;i++) {
-			vecdata->v_float64.mean[i] = ((double)(vecdata->v_float64.mean[i]))/(double)count;
-		}
-	} 
-}
-
 
 //recursive function to handle higher dimension matrices, by processing 2D sections at a time 
 void jit_artoolkit_calculate_ndim(t_jit_artoolkit *x, long dimcount, long *dim, long planecount, 
 	t_jit_matrix_info *in1_minfo, char *bip1)
 {
-	long i,j,n;
-	char *ip1;
-	t_jit_op_info in1_opinfo;
-	t_jit_artoolkit_vecdata vecdata;
-		
-	if (dimcount<1) return; //safety
-	
-	switch(dimcount) {
-	case 1:
-		dim[1] = 1;
-	case 2:
-		jit_artoolkit_precalc(&vecdata,in1_minfo,bip1); //parallel moved thread local
-		n = dim[0];
-		in1_opinfo.stride = in1_minfo->dim[0]>1?planecount:0;
-		if (in1_minfo->type==_jit_sym_char) {
-			for (i=0;i<dim[1];i++){
-				for (j=0;j<planecount;j++) {
-					in1_opinfo.p = bip1 + i*in1_minfo->dimstride[1] + j;
-					jit_artoolkit_vector_char(n,&in1_opinfo,&(vecdata.v_char.min[j]),
-						&(vecdata.v_char.mean[j]),&(vecdata.v_char.max[j]));
-				}
-			}
-			//parallel: protect access to global vecdata write
-			systhread_mutex_lock(x->mutex);
-			for (j=0;j<planecount;j++) {
-				if (vecdata.v_char.min[j]<x->vd.v_char.min[j])
-					x->vd.v_char.min[j] = vecdata.v_char.min[j];
-				if (vecdata.v_char.max[j]>x->vd.v_char.max[j])
-					x->vd.v_char.max[j] = vecdata.v_char.max[j];
-				x->vd.v_char.mean[j] += vecdata.v_char.mean[j];
-			}
-			systhread_mutex_unlock(x->mutex);
-		} else if (in1_minfo->type==_jit_sym_long) {
-			for (i=0;i<dim[1];i++){
-				for (j=0;j<planecount;j++) {
-					in1_opinfo.p = bip1 + i*in1_minfo->dimstride[1] + j*4;
-					jit_artoolkit_vector_long(n,&in1_opinfo,&(vecdata.v_long.min[j]),
-						&(vecdata.v_long.mean[j]),&(vecdata.v_long.max[j]));
-				}
-			}
-			//parallel: protect access to global vecdata write
-			systhread_mutex_lock(x->mutex);
-			for (j=0;j<planecount;j++) {
-				if (vecdata.v_long.min[j]<x->vd.v_long.min[j])
-					x->vd.v_long.min[j] = vecdata.v_long.min[j];
-				if (vecdata.v_long.max[j]>x->vd.v_long.max[j])
-					x->vd.v_long.max[j] = vecdata.v_long.max[j];
-				x->vd.v_long.mean[j] += vecdata.v_long.mean[j];
-			}
-			systhread_mutex_unlock(x->mutex);
-		} else if (in1_minfo->type==_jit_sym_float32) {
-			for (i=0;i<dim[1];i++){
-				for (j=0;j<planecount;j++) {
-					in1_opinfo.p = bip1 + i*in1_minfo->dimstride[1] + j*4;
-					jit_artoolkit_vector_float32(n,&in1_opinfo,&(vecdata.v_float32.min[j]),
-						&(vecdata.v_float32.mean[j]),&(vecdata.v_float32.max[j]));
-				}
-			}
-			//parallel: protect access to global vecdata write
-			systhread_mutex_lock(x->mutex);
-			for (j=0;j<planecount;j++) {
-				if (vecdata.v_float32.min[j]<x->vd.v_float32.min[j])
-					x->vd.v_float32.min[j] = vecdata.v_float32.min[j];
-				if (vecdata.v_float32.max[j]>x->vd.v_float32.max[j])
-					x->vd.v_float32.max[j] = vecdata.v_float32.max[j];
-				x->vd.v_float32.mean[j] += vecdata.v_float32.mean[j];
-			}
-			systhread_mutex_unlock(x->mutex);
-		} else if (in1_minfo->type==_jit_sym_float64) {
-			for (i=0;i<dim[1];i++){
-				for (j=0;j<planecount;j++) {
-					in1_opinfo.p = bip1 + i*in1_minfo->dimstride[1] + j*8;
-					jit_artoolkit_vector_float64(n,&in1_opinfo,&(vecdata.v_float64.min[j]),
-						&(vecdata.v_float64.mean[j]),&(vecdata.v_float64.max[j]));
-				}
-			}
-			//parallel: protect access to global vecdata write
-			systhread_mutex_lock(x->mutex);
-			for (j=0;j<planecount;j++) {
-				if (vecdata.v_float64.min[j]<x->vd.v_float64.min[j])
-					x->vd.v_float64.min[j] = vecdata.v_float64.min[j];
-				if (vecdata.v_float64.max[j]>x->vd.v_float64.max[j])
-					x->vd.v_float64.max[j] = vecdata.v_float64.max[j];
-				x->vd.v_float64.mean[j] += vecdata.v_float64.mean[j];
-			}
-			systhread_mutex_unlock(x->mutex);
-		} 
-		break;
-	default:
-		for	(i=0;i<dim[dimcount-1];i++) {
-			ip1 = bip1 + i*in1_minfo->dimstride[dimcount-1];
-			jit_artoolkit_calculate_ndim(x,dimcount-1,dim,planecount,in1_minfo,ip1);
-		}
-	}
-}
 
-//outmatrix is guaranteed to be no smaller than 256 elements so no need to test ip1 for 0-maxsize
-void jit_artoolkit_vector_char(long n, t_jit_op_info *in1, long *min, long *mean, long *max) 
-{
-	uchar *ip1;
-	long tmp;
-	long is1;
-		
-	ip1 = ((uchar *)in1->p);
-	is1 = in1->stride; 
-	
-	++n;
-	while (--n) {
-		tmp = *ip1;
-		if (tmp<(*min)) *min = tmp;
-		if (tmp>(*max)) *max = tmp;
-		*mean += tmp;
-		ip1 += is1;
-	}
-}
-
-void jit_artoolkit_vector_long(long n, t_jit_op_info *in1, long *min, double *mean, long *max) 
-{
-	t_int32 *ip1,tmp;
-	long is1;
-		
-	ip1 = ((t_int32 *)in1->p);
-	is1 = in1->stride; 
-	
-	++n;
-	while (--n) {
-		tmp = *ip1;
-		if (tmp<(*min)) *min = tmp;
-		if (tmp>(*max)) *max = tmp;
-		*mean += tmp;
-		ip1 += is1;
-	}
-}
-
-void jit_artoolkit_vector_float32(long n, t_jit_op_info *in1, float *min, float *mean, float *max) 
-{
-	float *ip1,tmp;
-	long is1;
-		
-	ip1 = ((float *)in1->p);
-	is1 = in1->stride; 
-	
-	++n;
-	while (--n) {
-		tmp = *ip1;
-		if (tmp<(*min)) *min = tmp;
-		if (tmp>(*max)) *max = tmp;
-		*mean += tmp;
-		ip1 += is1;
-	}
-}
-
-void jit_artoolkit_vector_float64(long n, t_jit_op_info *in1, double *min, double *mean, double *max) 
-{
-	double *ip1,tmp;
-	long is1;
-		
-	ip1 = ((double *)in1->p);
-	is1 = in1->stride; 
-	
-	++n;
-	while (--n) {
-		tmp = *ip1;
-		if (tmp<(*min)) *min = tmp;
-		if (tmp>(*max)) *max = tmp;
-		*mean += tmp;
-		ip1 += is1;
-	}
 }
 
 t_jit_artoolkit *jit_artoolkit_new(void)
